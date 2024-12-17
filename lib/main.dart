31ca0bcbfeef619391426e85f62b4bb35f2c1386
ifox6677 <zhangjq0908@outlook.com>
2024-12-17 21:40:30 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -196,7 +196,7 @@ Future<void> initialisation() async {
       builder: MusifyAudioHandler.new,
       config: const AudioServiceConfig(
         androidNotificationChannelId: 'com.gokadzev.musify',
-        androidNotificationChannelName: 'Musify',        
+        androidNotificationChannelName: 'afa-Music',        
         androidNotificationIcon: 'drawable/ic_launcher_foreground',
         androidShowNotificationBadge: true,
       ),
